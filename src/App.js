74a7f8f108557826a57e881581c16079d4f15d04
Fallen-Angel-FF <zhamichenok@gmail.com>
2022-05-14 19:54:05 +0300

    fix Routes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import './Styles/App.css'
-import {BrowserRouter, Route} from "react-router-dom";
+import {BrowserRouter, Route,} from "react-router-dom";
 import Posts from "./Pages/Posts";
 import About from "./Pages/About";
+import {Routes} from "react-router";
+
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/about">
-                    <About/>
-                </Route>
-                <Route path={"/posts"}>
-                    <Posts/>
-                </Route>
+                <Route path="/about" element={<About/>}/>
+                <Route path={"/posts"} element={<Posts/>}/>
             </Routes>
         </BrowserRouter>
     )
